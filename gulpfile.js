c666d00a54dabf2b14258bd33f6e0e977839e547
copiner <wdaonngg@gmail.com>
2019-09-10 13:53:12 +0800

    un

--- gulpfile.js
+++ gulpfile.js
@@ -16,21 +16,43 @@ const proxy = require('http-proxy-middleware');//反向代理
 const fileinclude = require('gulp-file-include');
 const rev = require('gulp-rev');
 const revCollector = require('gulp-rev-collector');
-// const rev = require('gulp-rev-append');//(?:href|src)="(.*)[\?]rev=(.*)[\"]   //版本号
+// const rev = require('gulp-rev-append');//(?:href|src)="(.*)[\?]rev=(.*)[\"]   //版本号 该方法舍弃
 const del = require('del');
 
 // NODE_ENV
 var env = process.env.NODE_ENV || 'development';
 var condition = env === 'production';
 
+task('css_dev', function (cb) {
+    src('src/scss/*.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('app/css'))
+    .pipe(connect.reload());
+    cb();
+});
 
-task('lib', function (cb) {
-  src('src/lib/*')
-  .pipe(dest('app/lib'));
-  cb();
-})
+task('html_dev', function (cb) {
+    src('src/*.html')
+    .pipe(plumber())
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+      }))
+    .pipe(dest('app'))
+    .pipe(connect.reload());
+    cb();
+});
 
-task('css_min', function (cb) {
+task('js_dev', function (cb) {
+    src('src/js/*.js')
+    .pipe(plumber())
+    .pipe(dest('app/js'))
+    .pipe(connect.reload());
+    cb();
+});
+
+task('css_pro', function (cb) {
     src('src/scss/*.scss')
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
@@ -38,64 +60,108 @@ task('css_min', function (cb) {
     .pipe(rev())
     .pipe(dest('app/css'))
     .pipe(rev.manifest())
-    .pipe(dest('rev/css'))
-    .pipe(connect.reload());
+    .pipe(dest('rev/css'));
     cb();
 });
 
-task('js_min', function (cb) {
+task('js_pro', function (cb) {
     src('src/js/*.js')
     .pipe(plumber())
-    .pipe(gulpif(condition, uglify()))
+    .pipe(uglify())
     .pipe(rev())
     .pipe(dest('app/js'))
     .pipe(rev.manifest())
-    .pipe(dest('rev/js'))
-    .pipe(connect.reload());
+    .pipe(dest('rev/js'));
     cb();
 });
 
-task('html_min', function (cb) {
+task('html_pro', function (cb) {
     let options = {
         removeComments: true,//清除HTML注释
         collapseWhitespace: true,//压缩HTML
         minifyJS: true,//压缩页面JS
         minifyCSS: true//压缩页面CSS
     };
-    src(['rev/**/*.json','src/index.html'])
+    src(['./rev/**/*.json','./src/*.html'])
     .pipe(plumber())
     .pipe(fileinclude({
         prefix: '@@',
         basepath: '@file'
       }))
     .pipe(revCollector({
+      collectedManifest:'sec-mainfest.json',
       replaceReved:true
     }))
-    //.pipe(htmlmin(options))
+    .pipe(htmlmin(options))
+    .pipe(revCollector({//需要两次  todo ？？？
+      collectedManifest:'sec-mainfest.json',
+      replaceReved:true
+    }))
     .pipe(dest('app'))
     .pipe(connect.reload());
     cb();
 });
 
+task('lib', function (cb) {
+  src('src/lib/*')
+  .pipe(dest('app/lib'));
+  cb();
+})
+
+task('config', function (cb) {
+  src('src/config/*')
+  .pipe(dest('app/config'));
+  cb();
+})
+
 task('image_min', function (cb) {
     src('src/imgs/*')
     .pipe(plumber())
-    //.pipe(gulpif(condition, imagemin()))
+    .pipe(gulpif(condition, imagemin()))
     .pipe(dest('app/imgs'));
     cb();
 });
 
 
 task('watch', function(cb){//监控
-  watch('./src/js/*.js',parallel('js_min'));
-  watch('./src/scss/*.scss',parallel('css_min'));
-  watch('./src/*.html',parallel('html_min'));
+
+  let watcher = watch(
+    ['./src/js/*.js','./src/scss/*.scss','./src/*.html'],
+    {events:['change','add','unlink']},
+    parallel('css_dev','js_dev','html_dev')
+  );
+
+  watcher.on('change', function(path, stats) {
+    console.log(`
+      ------------------------
+      File ${path} was changed
+      ------------------------
+      `);
+  });
+
+  watcher.on('add', function(path, stats) {
+    console.log(`
+      ----------------------
+      File ${path} was added
+      ----------------------
+      `);
+  });
+
+  watcher.on('unlink', function(path, stats) {
+    console.log(`
+      ------------------------
+      File ${path} was removed
+      ------------------------
+      `);
+  });
+
+  //watcher.close();
   cb();
 });
 
 
 task('clean', () => {
-  return del(['./app']).then(() => {
+  return del('./app').then(() => {
     console.log(`
         -----------------------------
           clean tasks are successful
@@ -107,7 +173,7 @@ task('clean', () => {
 
 
 //生成环境
-task('build', series('clean', parallel('lib','css_min','js_min','image_min'),'html_min',function(cb){
+task('build', series('clean', parallel('config','lib','css_pro','js_pro','image_min'),'html_pro',function(cb){
   console.log(`
       -----------------------------
         build tasks are successful
@@ -115,33 +181,34 @@ task('build', series('clean', parallel('lib','css_min','js_min','image_min'),'ht
       cb();
 }));
 
-task('server',series('build','watch',function(){
+//开发环境
+task('server',series('clean','watch',parallel('config','lib','css_dev','js_dev','image_min','html_dev'),function(){
     connect.server({
         root: 'app',
-        host:'localhost',
+        host:'192.168.1.77',
         port: 3000,
-        livereload: true
-        // middleware: function(connect, opt) {
-        //     return [
-        //         proxy('/api',  {
-        //             target: 'http://192.168.1.194:8080/sstsvr',
-        //             changeOrigin:true,
-        //             headers: {
-        //                  "Connection": "keep-alive"
-        //              },
-        //             //ws: true,
-        //             pathRewrite: {
-        //                 '^/api' : ''
-        //             },
-        //             router: {
-        //               // 'integration.localhost:3000' : 'http://localhost:8001',  // host only
-        //               // 'staging.localhost:3000'     : 'http://localhost:8002',  // host only
-        //               // 'localhost:3000/api'         : 'http://localhost:8003',  // host + path
-        //               // '/rest'                      : 'http://localhost:8004'   // path only
-        //             }
-        //         })
-        //     ]
-        // }
+        livereload: true,
+        middleware: function(connect, opt) {
+            return [
+                proxy('/api',  {
+                    target: 'http://192.168.23.142:8089/gateway',
+                    changeOrigin:true,
+                    headers: {
+                         "Connection": "keep-alive"
+                     },
+                    //ws: true,
+                    pathRewrite: {
+                        '^/api' : ''
+                    },
+                    router: {
+                      // 'integration.localhost:3000' : 'http://localhost:8001',  // host only
+                      // 'staging.localhost:3000'     : 'http://localhost:8002',  // host only
+                      // 'localhost:3000/api'         : 'http://localhost:8003',  // host + path
+                      // '/rest'                      : 'http://localhost:8004'   // path only
+                    }
+                })
+            ]
+        }
 
     });
     console.log(`
